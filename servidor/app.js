61b6e4c577e1adb7bebbaf69a333e0a7cd495fb3
Sie7he <diegoescurraromero@gmail.com>
2023-11-15 02:02:03 -0300

    cors

--- servidor/app.js
+++ servidor/app.js
@@ -1,9 +1,11 @@
 import express from "express";
-import morgan from 'morgan'
+import morgan from 'morgan';
+import cors from 'cors'
 import registro from './src/routes/registro.js'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
